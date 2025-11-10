cbf85f4bcffbbedab2f0dc885e2d00299b512843
wilaak <54738571+wilaak@users.noreply.github.com>
2025-11-10 12:52:34 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,92 @@
+
+<p align="center"><img width="150" height="150" src="https://data-star.dev/static/images/rocket-512x512.png"></p>
+
+# Datastar SDK for Swoldier
+
+This package offers an SDK for working with [Datastar](https://data-star.dev) in the [Swoldier](https://github.com/wilaak/swoldier) micro-framework.
+
+## Install 
+
+    composer require wilaak/datastar-swoldier
+
+## Usage
+
+```PHP
+use Swoole\{Runtime, Http\Server};
+use Swoldier\{Router, HttpContext};
+
+use DatastarSwoldier\{SSE, ElementPatchMode};
+
+// Replace blocking PHP functions with coroutine-friendly versions
+Runtime::enableCoroutine();
+
+$server = new Server("0.0.0.0", 8082);
+$router = new Router($server);
+
+$router->map('GET', '/', function (HttpContext $ctx) {
+    $sse = new SSE($ctx);
+
+    $sse->patchElements('<h1>Welcome to Datastar with Swoldier!</h1>', [
+        'selector' => '#app',
+        'mode' => ElementPatchMode::Replace,
+    ]);
+});
+
+$server->start();
+```
+
+This example covers most of the usage possible with this SDK:
+
+```php
+use Swoole\{Runtime, Http\Server};
+use DatastarSwoldier\{SSE, ElementPatchMode};
+
+// Replace blocking PHP functions with coroutine-friendly versions
+Runtime::enableCoroutine();
+
+$server = new Server("0.0.0.0", 8082);
+$router = new Router($server);
+
+$router->map('GET', '/sse-endpoint', function (HttpContext $ctx) {
+
+    // Creates a new SSE instance.
+    $sse = new SSE($ctx);
+
+    // Reads signals from the request.
+    $signals = $sse->readSignals();
+
+    // Patches elements into the DOM.
+    $sse->patchElements('<div></div>', [
+        'selector' => '#my-div',
+        'mode' => ElementPatchMode::Append,
+        'useViewTransition' => true,
+    ]);
+
+    // Removes elements from the DOM.
+    $sse->removeElements('#my-div', [
+        'useViewTransition' => true,
+    ]);
+
+    // Patches signals.
+    $sse->patchSignals(['foo' => 123], [
+        'onlyIfMissing' => true,
+    ]);
+
+    // Executes JavaScript in the browser.
+    $sse->executeScript('console.log("Hello, world!")', [
+        'autoRemove' => true,
+        'attributes' => [
+            'type' => 'application/javascript',
+        ],
+    ]);
+
+    // Redirects the browser by setting the location to the provided URI.
+    $sse->location('/guide');
+});
+
+$server->start();
+```
+
+## License
+
+MIT
---
+++ src/SSE.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace DatastarSwoldier;
+
+use starfederation\datastar\enums\ElementPatchMode as OriginalElementPatchMode;
+
+use starfederation\datastar\events\{
+    EventInterface,
+    ExecuteScript,
+    Location,
+    PatchElements,
+    PatchSignals,
+    RemoveElements,
+};
+
+use starfederation\datastar\Consts;
+
+use Swoldier\HttpContext;
+
+/**
+ * Server-Sent Event (SSE) generator for Datastar Swoldier.
+ */
+class SSE
+{
+    /**
+     * Whether the response headers have been sent.
+     */
+    public bool $sentHeaders = false;
+
+    /**
+     * Constructor for the SSE generator.
+     *
+     * @param HttpContext $ctx The swoldier HTTP context containing the request and response objects.
+     */
+    public function __construct(
+        private HttpContext $ctx,
+    ) {}
+
+    /**
+     * Returns the signals sent in the incoming request.
+     */
+    public function readSignals(): array
+    {
+        $input = $this->ctx->getQueryParam(Consts::DATASTAR_KEY);
+        $signals = $input ? \json_decode($input, true) : [];
+        return \is_array($signals) ? $signals : [];
+    }
+
+    /**
+     * Send the SSE response headers if they have not been sent yet.
+     */
+    public function sendHeaders(): void
+    {
+        if ($this->sentHeaders) {
+            return;
+        }
+
+        $headers = [
+            'Cache-Control' => 'no-cache',
+            'Content-Type' => 'text/event-stream',
+            // Disable buffering for Nginx.
+            // https://nginx.org/en/docs/http/ngx_http_proxy_module.html#proxy_buffering
+            'X-Accel-Buffering' => 'no',
+        ];
+
+        // Connection-specific headers are only allowed in HTTP/1.1.
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection
+        $proto = $this->ctx->getProtocol();
+        if ($proto === 'HTTP/1.1') {
+            $headers['Connection'] = 'keep-alive';
+        }
+
+        foreach ($headers as $name => $value) {
+            $this->ctx->setHeader($name, $value);
+        }
+
+        // Initial SSE data to establish the stream.
+        $this->ctx->write(":\n\n");
+        $this->sentHeaders = true;
+    }
+
+    /**
+     * Patches HTML elements into the DOM and returns the resulting output.
+     *
+     * @param array{
+     *     selector?: string|null,
+     *     mode?: ElementPatchMode|string|null,
+     *     useViewTransition?: bool|null,
+     *     eventId?: string|null,
+     *     retryDuration?: int|null,
+     * } $options
+     */
+    public function patchElements(string $elements, array $options = []): string
+    {
+        $mode = $options['mode'] ?? null;
+
+        if ($mode) {
+            $options['mode'] = OriginalElementPatchMode::from($mode->value);
+        }
+
+        return $this->sendEvent(new PatchElements($elements, $options));
+    }
+
+    /**
+     * Patches signals and returns the resulting output.
+     */
+    public function patchSignals(array|string $signals, array $options = []): string
+    {
+        return $this->sendEvent(new PatchSignals($signals, $options));
+    }
+
+    /**
+     * Removes elements from the DOM and returns the resulting output.
+     *
+     * @param array{
+     *      eventId?: string|null,
+     *      retryDuration?: int|null,
+     *  } $options
+     */
+    public function removeElements(string $selector, array $options = []): string
+    {
+        return $this->sendEvent(new RemoveElements($selector, $options));
+    }
+
+    /**
+     * Executes JavaScript in the browser and returns the resulting output.
+     */
+    public function executeScript(string $script, array $options = []): string
+    {
+        return $this->sendEvent(new ExecuteScript($script, $options));
+    }
+
+    /**
+     * Redirects the browser by setting the location to the provided URI and returns the resulting output.
+     */
+    public function location(string $uri, array $options = []): string
+    {
+        return $this->sendEvent(new Location($uri, $options));
+    }
+
+    /**
+     * Sends an event using the Swoole HTTP response object and returns the resulting output.
+     */
+    protected function sendEvent(EventInterface $event): string
+    {
+        if (!$this->sentHeaders) {
+            $this->sendHeaders();
+        }
+        $output = $event->getOutput();
+        $this->ctx->write($output);
+        return $output;
+    }
+}
